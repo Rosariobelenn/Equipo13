f242a1c2730040556e7d476112b074710c9e13cc
Calebsenm <calebsenm@gmail.com>
2025-10-02 09:54:25 -0500

    backend

---
+++ backend/src/main/java/com/app/pyme_go/PymeGoApplication.java
@@ -0,0 +1,13 @@
+package com.app.pyme_go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PymeGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PymeGoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/app/pyme_go/PymeGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.pyme_go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PymeGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
