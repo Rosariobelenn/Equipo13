5d97b5e20875a4c2ab56cd9ac97472917134e1f9
fedecrer24 <fedeecrer@gmail.com>
2025-10-06 15:06:59 -0300

    login

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Register from "../pages/Register";
 import RegisterFlow from "../components/forms/Register/RegisterFlow";
+import Milogin from "../pages/Milogin";
 
 export default function AppRoutes() {
   return (
@@ -8,6 +9,7 @@ export default function AppRoutes() {
       <Route path='/' element={<>test</>} />
       <Route path='/register' element={<Register />} />
       <Route path='/registerFlow' element={<RegisterFlow />} />
+      <Route path='/milogin' element={<Milogin/>} />
     </Routes>
   );
 }
