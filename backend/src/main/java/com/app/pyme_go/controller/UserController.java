79f3892582c098d4ef0d08c2d747a2e286a38948
Calebsenm <calebsenm@gmail.com>
2025-10-08 12:49:57 -0500

    feat: add environment variables configuration

--- backend/src/main/java/com/app/pyme_go/controller/UserController.java
+++ backend/src/main/java/com/app/pyme_go/controller/UserController.java
@@ -2,7 +2,6 @@ package com.app.pyme_go.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    public String hello() {
+        return String.format("Hello ");
     }
 }
