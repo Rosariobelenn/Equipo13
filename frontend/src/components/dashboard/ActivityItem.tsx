a56e93107bcbb924340eb35282914f6e4761c9ac
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-30 11:45:20 -0300

    feat: adds COLOR_MAP to frontend/src/components/dashboard/ActivityItem.tsx

--- frontend/src/components/dashboard/ActivityItem.tsx
+++ frontend/src/components/dashboard/ActivityItem.tsx
@@ -1,9 +1,14 @@
+import { COLOR_MAP } from "../../constants/colorMap";
 import type { ActivityItemProps } from "../../types/dashboard.types";
 
 function ActivityItem({ color, title, timestamp }: ActivityItemProps) {
+  const spanColorClass = COLOR_MAP[color] || "bg-gray-500";
+
   return (
     <li className="flex items-center gap-3 bg-gray-100/60 rounded-lg p-4">
-      <span className={`w-2 h-2 bg-${color}-500 rounded-full`}></span>
+      <span
+        className={`w-[10px] h-[10px] ${spanColorClass} rounded-full`}
+      ></span>
       <div className="flex-1">
         <p className="text-gray-900">{title}</p>
         <time className="text-sm text-gray-400">{timestamp}</time>
