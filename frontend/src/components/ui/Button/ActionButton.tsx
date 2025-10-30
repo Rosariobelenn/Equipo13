0c6ca7e05ad2d764d87cafc37ffbdf5baa2779ba
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-30 17:09:27 -0300

    feat: adds new prop onClick to frontend/src/components/ui/Button/ActionButton.tsx

--- frontend/src/components/ui/Button/ActionButton.tsx
+++ frontend/src/components/ui/Button/ActionButton.tsx
@@ -4,9 +4,11 @@ const ActionButton: React.FC<ActionButtonProps> = ({
   icon: Icon,
   text,
   className,
+  onClick,
 }) => (
   <button
-    className={`md:w-48 flex items-center justify-center gap-2 px-4 py-2 rounded-sm transition-colors font-medium text-sm ${className}`}
+    onClick={onClick}
+    className={`md:w-48 flex items-center justify-center gap-2 px-4 py-2 rounded-sm transition-colors font-medium text-sm cursor-pointer ${className}`}
   >
     <Icon className="w-4 h-4" />
     {text}
