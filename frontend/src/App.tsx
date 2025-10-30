001b8b25907dbd0197dc82db7101165dc0c37db3
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-30 12:22:13 -0300

    feat: adds ScrollToTop.tsx to frontend/src/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/index";
+import ScrollToTop from "./components/ui/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
       <AppRoutes />
+      <ScrollToTop />
     </BrowserRouter>
   );
 }
